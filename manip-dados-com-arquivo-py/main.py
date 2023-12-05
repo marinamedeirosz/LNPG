17455fe7e1a6a137ad0c44527aa7fb271c6ccfee
Marina Medeiros <79525275+marinamedeirosz@users.noreply.github.com>
2023-12-05 14:23:41 -0300

    Add files via upload

--- manip-dados-com-arquivo-py/main.py
+++ manip-dados-com-arquivo-py/main.py
@@ -37,12 +37,60 @@ def pedeDados():
                 print("Telefone inválido")
             else:
                 break
-        
+
         escreveDados(nome, idade, sexo, telefone)
-                
+        #busca sexo
+        while True:
+            resposta = input("Deseja pesquisar por sexo? (S/N)")
+            if resposta == "S":
+                while True:
+                    sexoPesquisado = input("Deseja pesquisar M(masculino), F(feminino) ou O(outro)? ")
+                    if sexoPesquisado != "M" and sexoPesquisado != "F" and sexoPesquisado != "O":
+                        print("Sexo inválido")
+                    else:
+                        busca_usuario_pelo_sexo(sexoPesquisado)
+            elif resposta == "N":
+                break
+            else:
+                print("Resposta inválida")
+
+
+def imprimeDados():
+    with open("dados.txt", "r", encoding="utf-8") as arquivo:
+        for linha in arquivo.readlines():
+            listaCont = linha.split("|")
+            if listaCont[2] == "M":
+                sexo2 = "Masculino"
+            elif listaCont[2] == "F":
+                sexo2 = "Feminino"
+            else:
+                sexo2 = "Outro"
+            print(f"Nome: {listaCont[0]}")
+            print(f"Idade: {listaCont[1]} anos")
+            print(f"Sexo: {sexo2}")
+            print(f"Telefone: {listaCont[3]}")
         
+
+def busca_usuario_pelo_sexo(sexo):
+    with open("dados.txt", "r", encoding="utf-8") as arquivo:
+        usuarios = []
+        usuariosSelecionados = []
+        for linha in arquivo.readlines():
+            listaCont = linha.split("|")
+            usuarios.append(listaCont)
+        for i in usuarios:
+            if sexo in i:
+                usuariosSelecionados.append(i)
+        for j in usuariosSelecionados:
+            print(f"Nome: {j[0]}")
+            print(f"Idade: {j[1]} anos")
+            print(f"Sexo: {j[2]}")
+            print(f"Telefone: {j[3]}")
+
+
 def main():
     pedeDados()
+    imprimeDados()
     
     
 if __name__ == "__main__":
